b795c4475f33c44c569af260c20f293eb8c26db0
Md Rejaul Karim <mdrejaulkarimroman1@gmail.com>
2026-09-23 00:14:16 +0600

    Add variables and input practice

--- 01-basics/01_variables.py
+++ 01-basics/01_variables.py
@@ -1,5 +1,15 @@
+# Variables
 name = "Arnob"
 age = 21
 
+# Print the values of variables
 print("My name is", name)
-print("I am", age)
+print("I am", age, "years old")
+
+# Taking input from the user
+user_name = input("Please enter your name: ")
+user_age = input("Please enter your age: ")
+
+# Printing the user input
+print("Your name is", user_name)
+print("You are", user_age, "years old")
